7954be590eb728afd04b74dbd343ec55308c7b01
Euler Carvalho <eulercarvalho.se@gmail.com>
2021-05-31 20:46:42 -0300

    Initial Commit

---
+++ Animations/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Animations
+//
+//  Created by Euler Carvalho on 31/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
